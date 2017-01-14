2f41b64e2a0551aa5990dc2e1dc5d25505dcc62a
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2017-01-15 02:42:26 +0800

    Added comments

--- server/router.js
+++ server/router.js
@@ -4,7 +4,10 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-
+/*
+ * Creates a new SimpleRouter instance.
+ * @returns {function}
+ */
 function SimpleRouter () {
   if (!(this instanceof SimpleRouter)) {
     return new SimpleRouter();
@@ -13,7 +16,12 @@ function SimpleRouter () {
   this.map = {};
 }
 
-
+/*
+ * Listens the requests for url
+ * and writes to response.
+ * @param req {object} IncomingRequest
+ * @param res {object} OutgoingResponse
+ */
 SimpleRouter.prototype.listen = function (req, res) {
 
   let reqUrl = req.url;
@@ -26,7 +34,12 @@ SimpleRouter.prototype.listen = function (req, res) {
     this._writeFileToResponse(req, res, this.map[reqUrlPath]);
 };
 
-
+/*
+ * Writes a key value pair to the
+ * map object.
+ * @param urlPath {string} Url to serve the file.
+ * @param filePath {string} File to be served.
+ */
 SimpleRouter.prototype.sendFile = function (urlPath, filePath) {
 
   if (typeof urlPath !== 'string' || typeof filePath !== 'string')
@@ -38,7 +51,12 @@ SimpleRouter.prototype.sendFile = function (urlPath, filePath) {
   this.map[urlPath] = filePath;
 }
 
-
+/*
+ * Sends a not found page to the client.
+ * Checks the file system if the 404 file exists.
+ * If not sends a default response.
+ * @param res {object} OutgoingResponse
+ */
 SimpleRouter.prototype._notFound = function (res) {
 
   let errorPageTemp =
@@ -59,7 +77,11 @@ SimpleRouter.prototype._notFound = function (res) {
   res.end(errorPageTemp);
 };
 
-
+/*
+ * Writes a given file to the response.
+ * @param req {object} IncomingRequest
+ * @param res {object} OutgoingResponse
+ */
 SimpleRouter.prototype._writeFileToResponse = function (req, res, filePath) {
 
   let source = fs.createReadStream(filePath);
